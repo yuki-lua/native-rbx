bf168b6dfa68f7a6340354609b3a58d9958dff0a
yuki.lua <afganpakelaptop@gmail.com>
2026-07-02 18:40:52 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,5 @@
 repeat task.wait() until game:IsLoaded()
+if not shared.gid then shared.gid = nil end
 local Utility = loadstring(game:HttpGet('https://raw.githubusercontent.com/yuki-lua/native-rbx/refs/heads/main/utility/universal.lua'))()
 local Experience = {139566161526375, 71480482338212, 6872265039}
 
@@ -7,7 +8,9 @@ for _, v in pairs(Experience) do
     if not Status then continue end
     local success, result = pcall(function()
         loadstring(game:HttpGet('https://raw.githubusercontent.com/yuki-lua/native-rbx/refs/heads/main/games/' .. Id .. '.lua'))()
-        if not shared.gid then shared.gid = Id end
+        if shared.gid == nil then 
+            shared.gid = Id
+        end
     end)
     if not success then
         warn(result)
